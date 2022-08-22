d627f9b5c6d0dbaf20b2e912a8b7536db5c6eb6b
Aurelien Senn <aurelien.senn.web@gmail.com>
2022-08-22 16:37:23 +0200

    add like and dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -87,3 +87,61 @@ exports.getAllStuff = (req, res, next) => {
     }
   );
 };
+
+exports.likeThing = (req, res) => {
+ 
+    if (req.body.like === 1) {
+      Sauce.updateOne({ _id: req.params.id },{
+          $inc: { likes: req.body.like++ },
+          $push: { usersLiked: req.body.userId },
+        }
+      )
+        .then((sauce) => res.status(200).json({ message: "Un like de plus !" }))
+        .catch((error) => res.status(400).json({ error }));
+    }
+  
+
+    else if (req.body.like === -1) {
+      Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { dislikes: req.body.like++ * -1 },
+          $push: { usersDisliked: req.body.userId },
+        }
+      )
+        .then((sauce) =>
+          res.status(200).json({ message: "Un dislike de plus !" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    }
+
+    else {
+      Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          if (sauce.usersLiked.includes(req.body.userId)) {
+            Sauce.updateOne(
+              { _id: req.params.id },
+              { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }
+            )
+              .then((sauce) => {
+                res.status(200).json({ message: "Un like de moins !" });
+              })
+              .catch((error) => res.status(400).json({ error }));
+          } else if (sauce.usersDisliked.includes(req.body.userId)) {
+            Sauce.updateOne(
+              { _id: req.params.id },
+              {
+                $pull: { usersDisliked: req.body.userId },
+                $inc: { dislikes: -1 },
+              }
+            )
+              .then((sauce) => {
+                res.status(200).json({ message: "Un dislike de moins !" });
+              })
+              .catch((error) => res.status(400).json({ error }));
+          }
+        })
+        .catch((error) => res.status(400).json({ error }));
+    }
+  };
+
