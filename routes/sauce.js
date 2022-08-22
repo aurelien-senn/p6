5be2b7852c51c0ae88e981870efceb1f166d0f5f
Aurelien Senn <aurelien.senn.web@gmail.com>
2022-08-22 16:45:25 +0200

    add like and dislike

--- routes/sauce.js
+++ routes/sauce.js
@@ -11,5 +11,6 @@ router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.post('/:id/like', auth, stuffCtrl.likeThing);
 
 module.exports = router;
